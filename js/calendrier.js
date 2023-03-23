0479cf7e4723a8d6a5649767117e33d4e3981d04
rkycrwlr <rkycrwlr@proton.me>
2023-03-23 21:34:16 +0100

    Page calendrier améliorée

--- js/calendrier.js
+++ js/calendrier.js
@@ -18,14 +18,18 @@ let th;
 for (let i = 0; i < jours.length; i++) {
     th = document.createElement("th")
     th.setAttribute("scope", "col")
+    th.setAttribute("class", "text-centered")
     th.appendChild(document.createTextNode(jours[i] + joursemaine.getDate() + "/" + (joursemaine.getMonth() + 1)))
     joursemaine.setDate(joursemaine.getDate() + 1)
     tr.appendChild(th)
 }
 
 events = [
-    ["Apéro", new Date(2023, 3, 22, 8, 0), new Date(2023, 3, 22, 9, 0)],
-    ["Goûter", new Date(2023, 3, 21, 18, 0), new Date(2023, 3, 21, 21, 0)],
+    ["Apéro", new Date(2023, 3, 20, 19, 0), new Date(2023, 3, 20, 21, 0)],
+    ["Nuit des défis", new Date(2023, 3, 22, 18, 0), new Date(2023, 3, 22, 23, 30)],
+    ["Nuit des défis", new Date(2023, 3, 23, 0, 0), new Date(2023, 3, 23, 6, 0)],
+    ["Petit déjeuner", new Date(2023, 3, 21, 6, 30), new Date(2023, 3, 21, 10, 0)],
+    ["Pot", new Date(2023, 3, 24, 22, 0), new Date(2023, 3, 24, 23, 30)]
 ]
 
 for(let i = 0; i < events.length; i++){
@@ -43,12 +47,20 @@ for(let i = 0; i < events.length; i++){
     }
     let dayEvent = beginDate.getDate()
     let idCol = dayEvent - debutsemaine.getDate()
+    console.log(debutsemaine.getDate())
 
-    td = getElementByXpath(`/html/body/table/tbody/tr[${idRow+1}]/td[${idCol}]`)
+    tr = getElementByXpath(`/html/body/section/table/tbody/tr[${idRow+1}]`)
+    let k = tr.children.length - 1
+    td = getElementByXpath(`/html/body/section/table/tbody/tr[${idRow+1}]/td[${Math.min(idCol+1,k)}]`)
     td.setAttribute("class", "td-event")
     td.setAttribute("rowspan", `${len}`)
     let text = document.createTextNode(events[i][0])
     td.appendChild(text)
+
+    for(let i=1; i<len; i++){
+        tr = getElementByXpath(`/html/body/section/table/tbody/tr[${idRow+1+i}]`)
+        tr.removeChild(tr.children[Math.min(idCol+1,k)])
+    }
 }
 
 
